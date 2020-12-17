ca852fefeba44dc8823e6ebfb474b9d9e91103f7
Joshua Hume <valencelectron@hotmail.com>
2020-12-17 18:59:13 +0900

    a little style

--- app/controllers/my_classes_controller.rb
+++ app/controllers/my_classes_controller.rb
@@ -30,9 +30,9 @@ class MyClassesController < ApplicationController
     end
     def create
         @user = User.find(session[:user_id])
-        @my_class = @user.my_classes.create(my_class_params)
-        if @my_class.valid?
-            redirect_to new_my_class_student_path(@my_class)
+        my_class = @user.my_classes.create(my_class_params)
+        if my_class.valid?
+            redirect_to edit_my_class_path(my_class)
         else
             flash[:alert] = 'That class name is already taken.'
             render "new"
